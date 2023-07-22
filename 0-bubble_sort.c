1b37bba58a3795a4608b7d15098fa63c5b2b0ff3
vagrant <vagrant@linux-vag20>
2023-07-22 19:15:19 +0000

    Fix print in task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		swap = false;
 
-		for (j = 0; j < (size - 1 - i); j++)
+		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -29,9 +29,9 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swap = true;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 		i++;
 	}
 }
